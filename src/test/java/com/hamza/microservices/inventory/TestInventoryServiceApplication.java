13628e97c139b39e09c9612fa8d8c23854e15928
hamzamassir <hamza.massir-etu@etu.univh2c.ma>
2024-10-18 01:36:22 +0100

    creating service

---
+++ src/test/java/com/hamza/microservices/inventory/TestInventoryServiceApplication.java
@@ -0,0 +1,11 @@
+package com.hamza.microservices.inventory;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestInventoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(InventoryServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
